1cf4f17cb6740baeec0b18caaee6c10d3160937d
Aliyu Abubakar <aliyufari@gmail.com>
2026-07-10 10:56:42 +0100

    Members to Aspirants Done

--- database/migrations/2026_07_06_045250_create_permission_tables.php
+++ database/migrations/2026_07_06_045250_create_permission_tables.php
@@ -54,7 +54,8 @@ return new class extends Migration
         Schema::create($tableNames['model_has_permissions'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotPermission, $teams) {
             $table->uuid($pivotPermission);
 
-            $table->string('model_type');
+            // $table->string('model_type');
+            $table->string('model_type', 125);
             $table->uuid($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
 
@@ -81,7 +82,8 @@ return new class extends Migration
         Schema::create($tableNames['model_has_roles'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotRole, $teams) {
             $table->uuid($pivotRole);
 
-            $table->string('model_type');
+            // $table->string('model_type');
+            $table->string('model_type', 125);
             $table->uuid($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
 
